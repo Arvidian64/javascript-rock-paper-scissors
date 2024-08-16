de9a630107c8a8b4e4083a2174de4dd165c5cfc4
Arvidian64 <[arvid.sthl@yahoo.com]>
2024-08-16 23:48:30 +0200

    Switch case returns the computerChoice, added scores for players

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 function getComputerChoice() {
     let randomChoice = Math.floor(Math.random() * 3) + 1;
     console.log(randomChoice)
+    switch (randomChoice) {
+        case 1:
+            return "Rock";
+        case 2:
+            return "Paper";
+        case 3:
+            return "Scissors";
+    };
 }
 
-function humanChoice(choice) {
-    console.log(choice)
+function humanChoice() {
+    let choice = prompt("Rock, Paper or Scissors?");
+    choice.charAt(0).toUpperCase() + choice.slice(1);
+    console.log(choice);
 }
 
-getComputerChoice();
-let choice = prompt("Rock, Paper or Scissors?");
-humanChoice(choice.charAt(0).toUpperCase() + choice.slice(1));
+let humanScore = 0;
+
+let computerScore = 0;
+
+let computerChoice = getComputerChoice();
+
+humanChoice();
