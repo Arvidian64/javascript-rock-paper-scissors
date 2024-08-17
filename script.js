178ce9a691038edfc3942708d1eed153059b1ec2
Arvidian64 <[arvid.sthl@yahoo.com]>
2024-08-17 13:01:44 +0200

    Retired my switch cases and used if else instead

--- script.js
+++ script.js
@@ -27,37 +27,45 @@ function playRound() {
 
     let humanChoice = getHumanChoice();
 
-    switch (computerChoice, humanChoice) {
-        case computerChoice == humanChoice:
-            console.log("It's a draw! Your "+humanChoice+" is the same as "+computerChoice);
-        case humanChoice == "Rock":
-            if (computerChoice == "Paper") {
-                console.log("You lose! "+computerChoice+" beats "+humanChoice);
-                computerScore ++;
-            }
-            else {
-                console.log("You win! "+humanChoice+" beats "+computerChoice);
-                humanScore ++;
-            }
-        case humanChoice == "Paper":
-            if (computerChoice == "Scissors") {
-                console.log("You lose! "+computerChoice+" beats "+humanChoice);
-                computerScore ++;
-            }
-            else {
-                console.log("You win! "+humanChoice+" beats "+computerChoice);
-                humanScore ++;
-            }
-        case humanChoice == "Scissors":
-            if (computerChoice == "Rock") {
-                console.log("You lose! "+computerChoice+" beats "+humanChoice);
-                computerScore ++;
-            }
-            else {
-                console.log("You win! "+humanChoice+" beats "+computerChoice);
-                humanScore ++;
-            }
+    console.log(computerChoice+" "+humanChoice)
+
+    if (computerChoice == humanChoice) {
+        console.log("It's a draw! Your "+humanChoice+" is the same as "+computerChoice);
     }
+
+    else if (humanChoice == "Rock") {
+        if (computerChoice == "Paper") {
+            console.log("You lose! "+computerChoice+" beats "+humanChoice);
+            computerScore ++;
+        }
+        else {
+            console.log("You win! "+humanChoice+" beats "+computerChoice);
+            humanScore ++;
+        }
+    }
+
+    else if (humanChoice == "Paper") {
+        if (computerChoice == "Scissors") {
+            console.log("You lose! "+computerChoice+" beats "+humanChoice);
+            computerScore ++;
+        }
+        else {
+            console.log("You win! "+humanChoice+" beats "+computerChoice);
+            humanScore ++;
+        }
+    }
+
+    else if ("Scissors") {
+        if (computerChoice == "Rock") {
+            console.log("You lose! "+computerChoice+" beats "+humanChoice);
+            computerScore ++;
+        }
+        else {
+            console.log("You win! "+humanChoice+" beats "+computerChoice);
+            humanScore ++;
+        }
+    }
+
 }
 
 playRound();
